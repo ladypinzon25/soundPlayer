c60a56159598f31b747e7393d0ca053254903c53
Lady Pinzon <lj.pinzon12@uniandes.edu.co>
2018-10-26 16:03:20 -0500

    Updates

---
+++ precache-manifest.df271c8f22262de2029039db4f7fb306.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f9cd1adbec22cadd88",
+    "url": "/soundPlayer/static/js/runtime~main.79f9cd1a.js"
+  },
+  {
+    "revision": "0aa09cc2deaaeb7631be",
+    "url": "/soundPlayer/static/js/main.0aa09cc2.chunk.js"
+  },
+  {
+    "revision": "35b2b2c4a1dcde3e6f19",
+    "url": "/soundPlayer/static/js/1.35b2b2c4.chunk.js"
+  },
+  {
+    "revision": "0aa09cc2deaaeb7631be",
+    "url": "/soundPlayer/static/css/main.8f27e130.chunk.css"
+  },
+  {
+    "revision": "82dad8a99796b1d1a6116a708cb8015b",
+    "url": "/soundPlayer/index.html"
+  }
+];
